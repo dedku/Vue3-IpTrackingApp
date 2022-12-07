6aaf936cb9b19b1830509a889b6c95ac085c38e4
Patryk Szabat <szabat.patryk@gmail..com>
2022-12-07 17:10:04 +0100

    router update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './../views/Home.vue'
 
+type Meta = {
+    title: string
+}
+
 type Routes = {
     path: string,
     name: string,
-    component: typeof Home
+    component: typeof Home,
+    meta: Meta
 }
 
 const routes: Routes[] = [
-    { path: '/', name: 'Home', component: Home }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            title: 'IP Adress Teacker'
+        }
+
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title}`;
+    next()
 })
 
 export default router
