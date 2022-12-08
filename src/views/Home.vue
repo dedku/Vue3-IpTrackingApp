ba5d16f60d1640bd5f8b2d5fed82644ee09dbea1
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-08 13:52:19 +0100

    Revert "Import and setup leaflet"
    This reverts commit 21ff23d64ea2778a769f52ebd0055088ad28de12.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col h-screen max-h-screen ">
-        <div class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32">
+        <div class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32">
             <div class="w-full max-w-screen-sm">
                 <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
                 <div class="flex">
@@ -12,31 +12,14 @@
             </div>
             <IPInfo />
         </div>
-        <div id="mapid" class="h-full z-10"></div>
     </div>
 
 </template>
 
-<script lang="ts">
+<script>
 import IPInfo from '../components/IPInfo.vue'
-// @ts-ignore
-import * as L from 'leaflet'
-import { onMounted } from 'vue';
 export default {
     name: 'Home',
-    components: { IPInfo },
-    setup() {
-        let mymap;
-
-        onMounted(() => {
-            mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-                accessToken: 'pk.eyJ1IjoidGVzdG9iamVjdCIsImEiOiJjbGJlenY1YmMwMWYwM3JvYmQydTdqNDA0In0.hVqLmkPaeJT7NKQDzSZg7g'
-            }).addTo(mymap);
-        })
-    }
+    components: { IPInfo }
 }
 </script>
